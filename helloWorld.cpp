f581ff88206b267709c141366c050f105bb8d66c
Zaida Hammel <zzaida.hammel@mths.ca>
2022-09-14 14:29:14 +0000

    added copyright comment

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+// Copyright (c) 2022 Zaida Hammel All rights reserved
+//
+// Created by Zaida Hammel
+// Created on September 2022
+// Displays "Hello, World!" in terminal.
 
 int main() {
     std::cout << "Hello, World!\n";
